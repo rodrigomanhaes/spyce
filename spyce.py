6e1505f5c4d48419e6d87e62ad1b0a08a7bb4ea1
Hugo Lopes Tavares <hltbra@gmail.com>
2009-07-10 16:33:41 -0300

    first simple real implementation to pass the tests

--- spyce.py
+++ spyce.py
@@ -1,7 +1,16 @@
 # SUT: Context
+import sys
 
 class Spec(object):
     def __enter__(self):
+        frame = sys._getframe(1)
+        frame_locals, frame_globals = frame.f_locals, frame.f_globals
+        self.context = frame_locals.get('context', None) or \
+                       frame_globals.get('context', None)
+
+        if self.context:
+            self.context.add_sub_context(self)
+
         return self
         
     def __exit__(self, type, value, error):
@@ -11,15 +20,19 @@ class Spec(object):
 class description(Spec):
     def __init__(self, klass):
         self._name = klass.__name__
+        self._sub_contexts = []
 
     def __exit__(self, type, value, error):
         print self._name + ':'
         self._print_sub_contexts()
     
-    def _print_sub_contexts(self):
-        print ' - should score 0 for gutter game'
+    def add_sub_context(self, sub_context):
+        self._sub_contexts.append(sub_context)
 
+    def _print_sub_contexts(self):
+        for sub_context in self._sub_contexts:
+            print ' - ' + sub_context.description
     
 class example(Spec):
     def __init__(self, textual_description):
-        pass
+        self.description = textual_description
