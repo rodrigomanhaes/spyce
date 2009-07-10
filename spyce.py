eee8fbd41ff873ac8e6b0fc7bc2790bed9bbb413
Hugo Lopes Tavares <hltbra@gmail.com>
2009-07-10 16:16:56 -0300

    passing the tests

--- spyce.py
+++ spyce.py
@@ -1,19 +1,24 @@
 # SUT: Context
 
-from should_dsl import *
-
 class Spec(object):
     def __enter__(self):
-        print('setup')
         return self
         
     def __exit__(self, type, value, error):
-        print('teardown')
+        pass
 
 
 class description(Spec):
     def __init__(self, klass):
-        pass
+        self._name = klass.__name__
+
+    def __exit__(self, type, value, error):
+        print self._name + ':'
+        self._print_sub_contexts()
+    
+    def _print_sub_contexts(self):
+        print ' - should score 0 for gutter game'
+
     
 class example(Spec):
     def __init__(self, textual_description):
