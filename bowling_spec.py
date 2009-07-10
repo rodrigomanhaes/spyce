5a813fbbe42afb0a2f18d582383af48c00b6ba2e
Hugo Lopes Tavares <hltbra@gmail.com>
2009-07-10 16:16:40 -0300

    put something relevant to pass the tests

--- bowling_spec.py
+++ bowling_spec.py
@@ -1,11 +1,11 @@
+from __future__ import with_statement
 from bowling import Bowling
-from spyce import description, it
+from spyce import description, example
+from should_dsl import should_be
 
-with description(Bowling) as context:
-    
-    with example("should score 0 for gutter game") as another_context:
-        another_context.foo |should_be| bar
-    
-    with example("2"):
-        pass
+with description(Bowling):
+    bowling = Bowling()
+
+    with example("should score 0 for gutter game"):
+        bowling.score |should_be| 0
 
